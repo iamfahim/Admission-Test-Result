d6c6d473f8bfb60467b5c821100af054b59c8f84
Mohimenol Islam Fahim <mislamfahim@gmail.com>
2019-03-10 14:43:02 +0600

    Add files via upload

---
+++ ATResult/inc/footer.php
@@ -0,0 +1,8 @@
+</section>
+<section class="footeroption">
+<p>© <?php echo date("Y"); ?> Admission Result</p>
+ <h2><?php echo "Software Engineering and Information System Design Lab"; ?></h2>
+</section>
+</div>
+</body>
+</html>
